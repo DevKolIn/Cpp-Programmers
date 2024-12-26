7877728de4ab0e3f39ba162f2b0ccf4f18076889
DevKolIn <donghyeon.study@gmail.com>
2024-12-26 11:03:25 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 1.12 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -1,9 +1,18 @@
 #include <string>
-#include <vector>
 
 using namespace std;
 
 int solution(string t, string p) {
-    int answer = 0;
-    return answer;
+    int result = 0;
+    int pSize = p.length();
+    long long pNum = stoll(p);
+
+    for (int i = 0; i <= t.length() - pSize; i++)
+    {
+        string subT = t.substr(i, pSize);   
+        if (stoll(subT) <= pNum)
+            result++;  
+    }
+     
+    return result;
 }
