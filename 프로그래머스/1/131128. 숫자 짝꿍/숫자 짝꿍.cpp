456f32f5316c1ed12cec6fd15626da5b9f59309e
DevKolIn <donghyeon.study@gmail.com>
2025-01-09 09:41:21 +0900

    [level 1] Title: 숫자 짝꿍, Time: 13.25 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 string solution(string X, string Y) {
-    // 숫자 0~9의 개수를 저장하는 배열
     vector<int> countX(10, 0);
     vector<int> countY(10, 0);
 
-    // X와 Y의 각 숫자의 개수 카운트
     for (char c : X) {
         countX[c - '0']++;
     }
@@ -16,20 +14,18 @@ string solution(string X, string Y) {
         countY[c - '0']++;
     }
 
-    // 공통으로 나타나는 숫자 추출
     string result = "";
-    for (int i = 9; i >= 0; --i) { // 큰 숫자부터 탐색
-        int commonCount = min(countX[i], countY[i]); // 짝지을 수 있는 개수
-        result += string(commonCount, '0' + i);     // 공통 숫자를 결과에 추가
+    for (int i = 9; i >= 0; --i) {
+        int commonCount = min(countX[i], countY[i]);
+        result += string(commonCount, '0' + i);
     }
 
-    // 결과 처리
     if (result.empty()) {
-        return "-1"; // 공통 숫자가 없으면 -1 반환
+        return "-1";
     }
     if (result[0] == '0') {
-        return "0"; // 결과가 0으로만 구성되면 0 반환
+        return "0";
     }
 
-    return result; // 짝꿍 반환
+    return result;
 }
