a6b27c07dfa088f4d05a385dbcd18f492123476a
DevKolIn <donghyeon.study@gmail.com>
2025-01-13 09:39:11 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+bool minCompare(int a, int b) {
+    if (b < 0) return true;
+    return a < b; 
+}
+
+int getAlphabetIdx(char ch)
+{
+    return ch - 'A';
+}
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    vector<int> answer;
+    vector<int> alphabetsDistance(26, -1);
+    
+    for (string str :keymap)
+    {
+        for (int idx = 0; idx < str.size(); idx++)
+        {
+            char alphabetIdx = getAlphabetIdx(str[idx]);
+       alphabetsDistance[alphabetIdx] = min(alphabetsDistance[alphabetIdx], idx + 1, minCompare);
+        }
+    }
+    
+
+    for (string target : targets)
+    {
+        int targetTotal = 0;
+        for (char ch : target)
+        {
+            char alphabetIdx = getAlphabetIdx(ch);
+            if (alphabetsDistance[alphabetIdx] == -1)
+            {
+                targetTotal = -1;
+                break;
+            }
+            else
+            {
+                targetTotal += alphabetsDistance[alphabetIdx];
+            }
+        }
+        answer.push_back(targetTotal);
+    }
+    
+    return answer;
+}
