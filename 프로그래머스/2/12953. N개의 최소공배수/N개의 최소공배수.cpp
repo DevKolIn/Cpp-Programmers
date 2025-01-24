571c6d7cdb8a58455593ab17e28436183bc02f35
DevKolIn <donghyeon.study@gmail.com>
2025-01-24 09:24:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.07 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+int primeSize = sizeof(primes) / sizeof(primes[0]);
+
+unordered_map<int, int> GetPrimeNumCounts(int num)
+{
+    unordered_map<int, int> map;
+    
+    while (num != 1)
+    {
+        for (int i = 0; i < primeSize; ++i)
+        {
+            if (num % primes[i] == 0)
+            {
+                map[primes[i]]++;
+                num /= primes[i];
+            }
+        }
+    }
+    
+    return map;
+}
+
+int solution(vector<int> arr) {
+    unordered_map<int, int> lcmPrimeNumCounts;
+    
+    for (int num : arr)
+    {
+       unordered_map<int, int> PrimeNumCounts = GetPrimeNumCounts(num);
+       for (auto& pair : PrimeNumCounts)
+       {
+           if (lcmPrimeNumCounts[pair.first] < pair.second)
+           {
+               cout << pair.first << " : " << pair.second << endl;
+               lcmPrimeNumCounts[pair.first] = pair.second;
+           }
+       }
+    }
+    
+    int answer = 1;
+    for (auto& pair : lcmPrimeNumCounts)
+    {
+        answer *= pow(pair.first, pair.second);
+    }    
+    
+    return answer;
+}
