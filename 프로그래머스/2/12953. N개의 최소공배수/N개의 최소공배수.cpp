ec2c7d8cc8189a8422bacf6bce89475686d5a3cc
DevKolIn <donghyeon.study@gmail.com>
2025-01-24 09:26:40 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 3.79 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <cmath>
-#include <iostream>
+
 using namespace std;
 
 int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
@@ -37,7 +37,6 @@ int solution(vector<int> arr) {
        {
            if (lcmPrimeNumCounts[pair.first] < pair.second)
            {
-               cout << pair.first << " : " << pair.second << endl;
                lcmPrimeNumCounts[pair.first] = pair.second;
            }
        }
