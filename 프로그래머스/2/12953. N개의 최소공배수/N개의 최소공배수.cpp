36428d90b57254b35e61d170872b8010e1edb3d0
DevKolIn <donghyeon.study@gmail.com>
2025-01-24 09:50:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,52 +1,14 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-int primeSize = sizeof(primes) / sizeof(primes[0]);
-
-unordered_map<int, int> GetPrimeNumCounts(int num)
-{
-    unordered_map<int, int> map;
-    
-    while (num != 1)
-    {
-        for (int i = 0; i < primeSize; ++i)
-        {
-            if (num % primes[i] == 0)
-            {
-                map[primes[i]]++;
-                num /= primes[i];
-            }
-        }
-    }
-    
-    return map;
-}
-
+int gcd(int x, int y) { return x % y == 0 ? y : gcd(y, x % y); }
+int lcm(int x, int y) { return x * y / gcd(x, y); }
 int solution(vector<int> arr) {
-    unordered_map<int, int> lcmPrimeNumCounts;
-    
-    for (int num : arr)
-    {
-       unordered_map<int, int> PrimeNumCounts = GetPrimeNumCounts(num);
-       for (auto& pair : PrimeNumCounts)
-       {
-           if (lcmPrimeNumCounts[pair.first] < pair.second)
-           {
-               lcmPrimeNumCounts[pair.first] = pair.second;
-           }
-       }
-    }
-    
-    int answer = 1;
-    for (auto& pair : lcmPrimeNumCounts)
-    {
-        answer *= pow(pair.first, pair.second);
-    }    
-    
+    int answer = arr[0];
+    for (int i = 1; i < arr.size(); i++)
+        answer = lcm(answer, arr[i]);
     return answer;
 }
