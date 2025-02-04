22c98ea56d22a895f79de0a2c0433eeeee5972dc
DevKolIn <donghyeon.study@gmail.com>
2025-02-04 09:47:05 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,9 +1,36 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
+
+    if (progresses.size() == 1)
+        return vector<int>(1,1);
+    
+    int count = 1;
+    int maxDaysLeft = ceil((100.f - progresses[0]) / speeds[0]);
+    for (int i = 1; i < progresses.size(); ++i)
+    {
+        int progress = progresses[i];
+        int speed = speeds[i];
+        int daysLeft = ceil((100.f - progress) / speed);
+
+        if (daysLeft > maxDaysLeft)
+        {
+            answer.push_back(count);
+            count = 1;
+            maxDaysLeft = daysLeft;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    
+    answer.push_back(count);
+    
     return answer;
 }
