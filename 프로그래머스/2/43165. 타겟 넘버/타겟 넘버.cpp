c0781f668a26d702baf95705d67e2bb68863ff7a
DevKolIn <donghyeon.study@gmail.com>
2025-02-07 09:28:28 +0900

    [level 2] Title: 타겟 넘버, Time: 5.77 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dfs(vector<int>& numbers, int idx, int target)
+{
+    if (idx >= numbers.size())
+    {
+        if (target == 0)
+            return 1;
+        return 0;
+    }
+
+    int positiveNum = target + numbers[idx];
+    int negativeNum = target - numbers[idx];
+    
+    return dfs(numbers, idx + 1, positiveNum) + dfs(numbers, idx + 1, negativeNum);
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    answer = dfs(numbers, 0, target);
+    return answer;
+}
