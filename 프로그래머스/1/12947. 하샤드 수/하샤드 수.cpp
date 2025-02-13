cb191d2f38227666dabb3d9deeac504c3cc95d45
DevKolIn <donghyeon.study@gmail.com>
2025-02-13 17:24:34 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool result = false;
+    int sum = 0;
+    int originalX = x;
+    
+    while (x > 0)
+    {
+        int remain = x % 10;
+        sum += remain;
+        x /= 10;
+    }
+    
+    if (originalX % sum == 0)
+        result = true;
+    
+    return result;
+}
