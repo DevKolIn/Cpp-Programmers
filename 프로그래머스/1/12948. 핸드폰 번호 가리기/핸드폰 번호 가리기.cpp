72b22441e52b0e4473fe6fe9b92d37f1d3ff24bf
DevKolIn <donghyeon.study@gmail.com>
2025-02-17 09:45:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    for (int idx = 0; idx < phone_number.size() - 4; idx++)
+    {
+        phone_number[idx] = '*';
+    }
+    
+    return phone_number;
+}
